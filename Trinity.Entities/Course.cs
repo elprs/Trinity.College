b025580885eb7f80e0b11fe3ae57d028c2a7827f
elprs <eleni.parisi.sunshine@gmail.com>
2020-03-26 07:29:15 +0200

    Add project files.

---
+++ Trinity.Entities/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trinity.Entities
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        [Required(ErrorMessage = "Required field"), MaxLength(50), MinLength(2)]
+        public string Title { get; set; }
+    }
+}
---
+++ Trinity.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Trinity.Web.MvcApplication" Language="C#" %>
