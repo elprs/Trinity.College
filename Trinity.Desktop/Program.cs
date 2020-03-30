d19b4c3824a65dc0d82796fc0c2200d3085b0a64
elprs <eleni.parisi.sunshine@gmail.com>
2020-03-30 23:59:37 +0300

    console nice printing

--- Trinity.Desktop/Program.cs
+++ Trinity.Desktop/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Data.Entity;
 using Trinity.Entities;
 using Trinity.Database;
+using System.Globalization;
 
 namespace Trinity.Desktop
 {
@@ -14,24 +15,32 @@ namespace Trinity.Desktop
         static void Main(string[] args)
         {
 
-
-
+            Console.OutputEncoding = Encoding.UTF8;
+            CultureInfo.CurrentCulture = CultureInfo.CreateSpecificCulture("gr-GR");
             MyDatabase db = new MyDatabase();
             foreach (var course in db.Courses.ToList())
             {
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.WriteLine("Course : " + course.Title);
                 
+
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("{0, 20} : {1, -20}", "Course title", course.Title);
+                Console.WriteLine("{0, 20} : {1:D}", "Course start date", course.StartDate);
+                Console.WriteLine("{0, 20} : {1, -20:C0}", "Course fee",  course.Fee);
+                Console.WriteLine("{0, 20} : {1, -20}", "Course description" , course.Description);
+                Console.WriteLine("{0, 20} : {1, -20}", "Course type" , course.Type);
+                Console.WriteLine();
+
+
 
                 foreach (var subject in course.Subjects)
                 {
                     Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine("\tSubject: " + subject.Title);
+                    Console.WriteLine("Subject: " + subject.Title);
 
                     foreach (var teacher in subject.Teachers)
                     {
                         Console.ForegroundColor = ConsoleColor.White;
-                        Console.WriteLine("\t\tTeacher : " + teacher.FirstName + " " + teacher.LastName);
+                        Console.WriteLine("\tTeacher : " + teacher.FirstName + " " + teacher.LastName);
                     }
 
                     foreach (var assignment in subject.Assignments)
@@ -43,45 +52,20 @@ namespace Trinity.Desktop
                             Console.ForegroundColor = ConsoleColor.DarkYellow;
                             Console.WriteLine(" {0, 40} {1} {2}", "Student : ", mark.Student.FirstName, mark.Student.LastName);
                             Console.ForegroundColor = ConsoleColor.White;
-                            Console.WriteLine(" {0, 40} {1}", "Assignment mark : ", mark.AssignmentMark);
+                            Console.WriteLine(" {0, 40} {1}", "Mark : ", mark.AssignmentMark);
                             Console.ForegroundColor = ConsoleColor.White;
+                          
                         }
+                      
                     }
                 }
+
+                Console.WriteLine("``````````````````````````````````````````````````````````````````````````````````````````");
             }
 
             Console.ReadKey();
 
-            //using (MyDatabase db = new MyDatabase())
-            //{
-            //    var subjects = db.Subjects.Include(x => x.Assignment);
-
-            //    foreach (var subject in subjects)
-            //    {
-            //        Console.WriteLine(subject.Title);
-            //        Console.Write("\t");
-            //        Console.WriteLine(subject.Assignment.Title);
-            //        Console.WriteLine("\n==========");
-            //    }
-            //}
-
-            //using (MyDatabase db = new MyDatabase())
-            //{
-            //    var assignments = db.Assignments.Include(x => x.Subject);
-
-            //    foreach (var assignment in assignments)
-            //    {
-            //        Console.WriteLine(assignment.Title);
-            //        Console.Write("\t");
-            //        Console.WriteLine(assignment.Subject.Title);
-            //        Console.WriteLine("\n==========");
-            //    }
-            //}
-
-
-
-
-            Console.ReadKey();
+          
         }
     }
 }
