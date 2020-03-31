7752f4fd549f818284dbd064589cff78826fdcd2
elprs <eleni.parisi.sunshine@gmail.com>
2020-03-31 13:00:34 +0300

    FirstTEstCtrllr

--- Trinity.Desktop/Program.cs
+++ Trinity.Desktop/Program.cs
@@ -15,55 +15,55 @@ namespace Trinity.Desktop
         static void Main(string[] args)
         {
 
-            //Console.OutputEncoding = Encoding.UTF8;
-            //CultureInfo.CurrentCulture = CultureInfo.CreateSpecificCulture("gr-GR");
-            //using (MyDatabase db = new MyDatabase())
-            //{
-            //    foreach (var course in db.Courses.ToList())
-            //    {
+            Console.OutputEncoding = Encoding.UTF8;
+            CultureInfo.CurrentCulture = CultureInfo.CreateSpecificCulture("gr-GR");
+            using (MyDatabase db = new MyDatabase())
+            {
+                foreach (var course in db.Courses.ToList())
+                {
 
 
-            //        Console.ForegroundColor = ConsoleColor.Cyan;
-            //        Console.WriteLine("{0, 20} : {1, -20}", "Course title", course.Title);
-            //        Console.WriteLine("{0, 20} : {1:D}", "Course start date", course.StartDate);
-            //        Console.WriteLine("{0, 20} : {1, -20:C0}", "Course fee", course.Fee);
-            //        Console.WriteLine("{0, 20} : {1, -20}", "Course description", course.Description);
-            //        Console.WriteLine("{0, 20} : {1, -20}", "Course type", course.Type);
-            //        Console.WriteLine();
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine("{0, 20} : {1, -20}", "Course title", course.Title);
+                    Console.WriteLine("{0, 20} : {1:D}", "Course start date", course.StartDate);
+                    Console.WriteLine("{0, 20} : {1, -20:C0}", "Course fee", course.Fee);
+                    Console.WriteLine("{0, 20} : {1, -20}", "Course description", course.Description);
+                    Console.WriteLine("{0, 20} : {1, -20}", "Course type", course.Type);
+                    Console.WriteLine();
 
 
 
-            //        foreach (var subject in course.Subjects)
-            //        {
-            //            Console.ForegroundColor = ConsoleColor.Yellow;
-            //            Console.WriteLine("Subject: " + subject.Title);
+                    foreach (var subject in course.Subjects)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("Subject: " + subject.Title);
 
-            //            foreach (var teacher in subject.Teachers)
-            //            {
-            //                Console.ForegroundColor = ConsoleColor.White;
-            //                Console.WriteLine("\tTeacher : " + teacher.FirstName + " " + teacher.LastName);
-            //            }
+                        foreach (var teacher in subject.Teachers)
+                        {
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.WriteLine("\tTeacher : " + teacher.FirstName + " " + teacher.LastName);
+                        }
 
-            //            foreach (var assignment in subject.Assignments)
-            //            {
-            //                Console.ForegroundColor = ConsoleColor.DarkGreen;
-            //                Console.WriteLine(" {0, 40} {1}", "Assignment title : ", assignment.Title);
-            //                foreach (var mark in assignment.Marks)
-            //                {
-            //                    Console.ForegroundColor = ConsoleColor.DarkYellow;
-            //                    Console.WriteLine(" {0, 40} {1} {2}", "Student : ", mark.Student.FirstName, mark.Student.LastName);
-            //                    Console.ForegroundColor = ConsoleColor.White;
-            //                    Console.WriteLine(" {0, 40} {1}", "Mark : ", mark.AssignmentMark);
-            //                    Console.ForegroundColor = ConsoleColor.White;
+                        foreach (var assignment in subject.Assignments)
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkGreen;
+                            Console.WriteLine(" {0, 40} {1}", "Assignment title : ", assignment.Title);
+                            foreach (var mark in assignment.Marks)
+                            {
+                                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                                Console.WriteLine(" {0, 40} {1} {2}", "Student : ", mark.Student.FirstName, mark.Student.LastName);
+                                Console.ForegroundColor = ConsoleColor.White;
+                                Console.WriteLine(" {0, 40} {1}", "Mark : ", mark.AssignmentMark);
+                                Console.ForegroundColor = ConsoleColor.White;
 
-            //                }
+                            }
 
-            //            }
-            //        }
+                        }
+                    }
 
-            //        Console.WriteLine("``````````````````````````````````````````````````````````````````````````````````````````");
-            //    }
-            //}
+                    Console.WriteLine("``````````````````````````````````````````````````````````````````````````````````````````");
+                }
+            }
 
             Console.ReadKey();
 
---
+++ Trinity.Web/Controllers/TestCoursesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Trinity.Database;
+using Trinity.Entities;
+
+namespace Trinity.Web.Controllers
+{
+    public class TestCoursesController : Controller
+    {
+        private MyDatabase db = new MyDatabase();
+
+        // GET: TestCourses
+        public ActionResult Index()
+        {
+            return View(db.Courses.ToList());
+        }
+
+        // GET: TestCourses/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Course course = db.Courses.Find(id);
+            if (course == null)
+            {
+                return HttpNotFound();
+            }
+            return View(course);
+        }
+
+        // GET: TestCourses/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TestCourses/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CourseId,Title,StartDate,EndDate,Type,Description,PhotoURL,Fee")] Course course)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Courses.Add(course);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(course);
+        }
+
+        // GET: TestCourses/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Course course = db.Courses.Find(id);
+            if (course == null)
+            {
+                return HttpNotFound();
+            }
+            return View(course);
+        }
+
+        // POST: TestCourses/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CourseId,Title,StartDate,EndDate,Type,Description,PhotoURL,Fee")] Course course)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(course).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(course);
+        }
+
+        // GET: TestCourses/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Course course = db.Courses.Find(id);
+            if (course == null)
+            {
+                return HttpNotFound();
+            }
+            return View(course);
+        }
+
+        // POST: TestCourses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Course course = db.Courses.Find(id);
+            db.Courses.Remove(course);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
