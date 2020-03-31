94c54704474e6b07c4e4b8a6f036bec3af8aab6c
elprs <eleni.parisi.sunshine@gmail.com>
2020-03-31 17:54:21 +0300

    public classesRepositories

---
+++ Trinity.Services/StudentRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trinity.Database;
+
+namespace Trinity.Services
+{
+    public class StudentRepository
+    {
+    }
+}
---
+++ Trinity.Services/TeacherRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Trinity.Database;
+using System.Threading.Tasks;
+
+namespace Trinity.Services
+{
+   public class TeacherRepository
+    {
+    }
+}
