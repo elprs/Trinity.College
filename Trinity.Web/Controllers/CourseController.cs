5bf1a2b7531ed9e6b74a2e79ae1c430100cb58e0
elprs <eleni.parisi.sunshine@gmail.com>
2020-04-01 19:18:16 +0300

    courseCtrllr

--- Trinity.Web/Controllers/CourseController.cs
+++ Trinity.Web/Controllers/CourseController.cs
@@ -12,14 +12,24 @@ namespace Trinity.Web.Controllers
     public class CourseController : Controller
     {
         // GET: Course
-        public ActionResult CourseTables(string sortOrder)
+        public ActionResult CourseTables(string sortOrder, string searchTitle, string searchType, string searchStartDate, string searchEndDate, double? searchFee, int? page, int? pSize)
         {
+
+            ViewBag.CurrentTitle = searchTitle;
+            ViewBag.CurrentType = searchType;
+            ViewBag.CurrentStartDate = searchStartDate;
+            ViewBag.CurrentEndDate = searchEndDate;
+            ViewBag.CurrentFee = searchFee;
+            ViewBag.CurrentSortOrder = sortOrder;
+            ViewBag.CurrentpSize = pSize;
 
             ViewBag.TitleSortParam = String.IsNullOrEmpty(sortOrder) ? "TitleDesc" : "";
             ViewBag.TypeSortParam = sortOrder == "TypeAsc" ? "TypeDesc" : "TypeAsc";
             ViewBag.StartDateSortParam = sortOrder == "StartDateAsc" ? "StartDateDesc" : "StartDateAsc";
             ViewBag.EndDateSortParam = sortOrder == "EndDateAsc" ? "EndDateDesc" : "EndDateAsc";
             ViewBag.FeeSortParam = sortOrder == "FeeAsc" ? "FeeDesc" : "FeeAsc";
+            ViewBag.DetailsSortParam = sortOrder == "DetailsAsc" ? "DetailsDesc" : "DetailsAsc";
+
 
             ViewBag.TiPrimary = "btn-sm btn-primary";
             ViewBag.TyPrimary = "btn-sm btn-primary";
@@ -32,6 +42,33 @@ namespace Trinity.Web.Controllers
             CourseRepository cr = new CourseRepository();
             var courses = cr.GetAll();
             cr.Dispose();
+
+
+            ////======================FILTERS===============================
+            ////Filtering  FirstName
+            //if (!string.IsNullOrWhiteSpace(searchfirstname))
+            //{
+            //    Directors = Directors.Where(x => x.FirstName.ToUpper().Contains(searchfirstname.ToUpper()));
+            //}
+            ////Filtering  LastName
+            //if (!string.IsNullOrWhiteSpace(searchlastname))
+            //{
+            //    Directors = Directors.Where(x => x.LastName.ToUpper().Contains(searchlastname.ToUpper()));
+            //}
+            ////Filtering  Minimum Age
+            //if (!(searchminage is null)) //40
+            //{
+            //    Directors = Directors.Where(x => x.Age >= searchminage);
+            //}
+            ////Filtering  Maximum Age
+            //if (!(searchmaxage is null)) //50
+            //{
+            //    Directors = Directors.Where(x => x.Age <= searchmaxage);
+            //}
+
+            ////Sorting
+
+
 
             switch (sortOrder)
             {
@@ -51,12 +88,16 @@ namespace Trinity.Web.Controllers
                 default: courses = courses.OrderBy(x => x.Title); ViewBag.TiPrimary = "btn-sm btn-primary"; break;
             }
 
+            //int pageSize = pSize ?? 3;
+            //int pageNumber = page ?? 1;  //nullable coehelesing operator
+            //return View(Directors.ToPagedList(pageNumber, pageSize));
+
             return View(courses);
         }
 
     // GET: TestCourses/Details/5
-    public ActionResult SoloDetails(int? id)
-    {
+    public ActionResult SoloDetails(int? id)//       public ActionResult SimpleDetails(int? id)
+        {
             CourseRepository cr = new CourseRepository();
             if (id == null)
         {
