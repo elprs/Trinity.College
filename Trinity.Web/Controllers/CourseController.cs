f81b22c5097556af30ca8955982f65c1fa38d4d1
elprs <eleni.parisi.sunshine@gmail.com>
2020-04-01 17:27:55 +0300

    courceTables ready

--- Trinity.Web/Controllers/CourseController.cs
+++ Trinity.Web/Controllers/CourseController.cs
@@ -12,11 +12,44 @@ namespace Trinity.Web.Controllers
     public class CourseController : Controller
     {
         // GET: Course
-        public ActionResult CourseTables()
+        public ActionResult CourseTables(string sortOrder)
         {
+
+            ViewBag.TitleSortParam = String.IsNullOrEmpty(sortOrder) ? "TitleDesc" : "";
+            ViewBag.TypeSortParam = sortOrder == "TypeAsc" ? "TypeDesc" : "TypeAsc";
+            ViewBag.StartDateSortParam = sortOrder == "StartDateAsc" ? "StartDateDesc" : "StartDateAsc";
+            ViewBag.EndDateSortParam = sortOrder == "EndDateAsc" ? "EndDateDesc" : "EndDateAsc";
+            ViewBag.FeeSortParam = sortOrder == "FeeAsc" ? "FeeDesc" : "FeeAsc";
+
+            ViewBag.TiPrimary = "btn-sm btn-primary";
+            ViewBag.TyPrimary = "btn-sm btn-primary";
+            ViewBag.SdPrimary = "btn-sm btn-primary";
+            ViewBag.EdPrimary = "btn-sm btn-primary";
+            ViewBag.FePrimary = "btn-sm btn-primary";
+            
+           
+
             CourseRepository cr = new CourseRepository();
             var courses = cr.GetAll();
             cr.Dispose();
+
+            switch (sortOrder)
+            {
+                case "TitleDesc": courses = courses.OrderByDescending(x => x.Title); ViewBag.TiPrimary = "btn-sm btn-success"; break; 
+                case "TypeAsc": courses = courses.OrderBy(x => x.Type); ViewBag.TyPrimary = "btn-sm btn-primary"; break;
+                case "TypeDesc": courses = courses.OrderByDescending(x => x.Type); ViewBag.TyPrimary = "btn-sm btn-success"; break;
+                case "StartDateAsc": courses = courses.OrderBy(x => x.StartDate); ViewBag.SdPrimary = "btn-sm btn-primary"; break;
+                case "StartDateDesc": courses = courses.OrderByDescending(x => x.StartDate); ViewBag.SdPrimary = "btn-sm btn-success"; break;
+                case "EndDateAsc": courses = courses.OrderBy(x => x.EndDate); ViewBag.EdPrimary = "btn-sm btn-primary"; break;
+                case "EndDateDesc": courses = courses.OrderByDescending(x => x.EndDate); ViewBag.EdPrimary = "btn-sm btn-success"; break;
+                case "FeeAsc": courses = courses.OrderBy(x => x.Fee); ViewBag.FePrimary = "btn-sm btn-primary"; break;
+                case "FeeDesc": courses = courses.OrderByDescending(x => x.Fee); ViewBag.FePrimary = "btn-sm btn-success"; break;
+
+
+
+
+                default: courses = courses.OrderBy(x => x.Title); ViewBag.TiPrimary = "btn-sm btn-primary"; break;
+            }
 
             return View(courses);
         }
