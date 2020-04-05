7c41b24a8939caf3dba870472dc06d3f928f0130
elprs <eleni.parisi.sunshine@gmail.com>
2020-04-05 15:49:58 +0300

    create-details course

---
+++ Trinity.Web/Controllers/CourseCRUDController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Trinity.Entities;
+using Trinity.Services;
+
+namespace Trinity.Web.Controllers
+{
+    public class CourseCRUDController : Controller
+    {
+        
+        // GET: CourseCRUD
+
+        public ActionResult CourseCrudIndex()
+        {
+            CourseRepository courseRepository = new CourseRepository();
+
+            var courses = courseRepository.GetAll();
+
+            return View(courses);
+        }
+
+
+        // GET: TestCourses/Details/5
+        public ActionResult Details(int? id)
+        {
+            CourseRepository courseRepository = new CourseRepository();
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Course course = courseRepository.GetById(id);
+
+            if (course == null)
+            {
+                return HttpNotFound();
+            }
+            return View(course);
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
--- Trinity.Web/Controllers/StatsController.cs
+++ Trinity.Web/Controllers/StatsController.cs
@@ -51,13 +51,39 @@ namespace Trinity.Web.Controllers
 
             return View(vm);
         }
-        //public  ActionResult ThreeTableModelStatsTable()
-        //{
-        //    AllEntities vm = new AllEntities();
+        public ActionResult ThreeTableModelStatsTable()
+        {
+            AllEntities vm = new AllEntities();
+
+
+            //var person = (from p in PersonBooks
+            //              join b in Book on p.BookId equals b.BookId
+            //              where b.BookId  IN(2, 3, 4)
+            //      select new { p.PersonId });
+
+
+            //var query = (from s in vm.Students
+            //             join m in vm.Marks on s.StudentId equals m.StudentId
+            //             join cs in vm.CourseStudents on s.StudentId equals cs.StudentId
+            //            select 
+
+            //from p in Context.Book
+            //where (Context.PersonBooks.Where(x => personIds.Contains(x.PersonId))
+            //            .GroupBy(x => x.BookId)
+            //            .Where(x => x.Count() == personIds.Count)
+            //            .OrderByDescending(x => x.Count())
+            //            .Select(x => x.Key).ToList()).Contains(p.BookId)
+            //select p;
 
 
 
-        //    return View(vm);
-        //}
+            //var query = from student in vm.Students
+            //                where (  vm.CourseStudents.Where(x=> x.StudentId == student.StudentId)
+            //                .GroupBy (x=>vm.Assignments.)
+
+            //            select p;
+
+            return View(vm);
+        }
     }
 }
